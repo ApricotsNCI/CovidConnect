918502fdbd6450a3c7c9b2991e7861c83d35cf7e
ideed <emanuelivan45@gmail.com>
2020-10-25 12:52:37 +0000

    Database fixes 12

--- dataphp.php
+++ dataphp.php
@@ -5,28 +5,26 @@ $username = "b66c12d2a6df51";
 $password = "42536d71";
 $db = "heroku_ee0928b4ad437e0";
 
+$conn = new mysqli($server, $username, $password, $db);
+
 $user = $conn->real_escape_string($_POST[username]);
 $pass = $conn->real_escape_string($_POST[password]);
-
-$conn = new mysqli($server, $username, $password, $db);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-
-$result = $conn -> query($sql);
-
-$row = $result -> fetch_array(MYSQLI_ASSOC);
-
+$sql = "SELECT id FROM admin WHERE username = '$user' and passcode = '$pass'";
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 $active = $row['active'];
 
 $count = mysqli_num_rows($result);
 
 if($count == 1) {
-         $echo "Login successful! Welcome "+$user+"!";
+         echo "Login successful! Welcome "+$user+"!";
       }else {
-         $echo "Your Login Name or Password is invalid";
+         echo "Your Login Name or Password is invalid";
 }
+
 ?>
