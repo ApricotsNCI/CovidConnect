81d30a9e70d958c7bb42bfbb02c82fbb7adfdbb2
lukekavanagh209 <kavanaghluke19@gmail.com>
2020-10-27 18:25:36 +0000

    Connected profile and connections

---
+++ dataphp.php
@@ -0,0 +1,30 @@
+<?php
+
+$server = "eu-cdbr-west-03.cleardb.net";
+$user = "b66c12d2a6df51";
+$pass = "42536d71";
+$db = "heroku_ee0928b4ad437e0";
+
+$conn = new mysqli($server, $user, $pass, $db);
+
+$username = mysqli_real_escape_string($conn,$_POST['username']);
+$password = mysqli_real_escape_string($conn,$_POST['password']);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT idusers FROM users WHERE username = '$username' and password = '$password'";
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+$active = $row['active'];
+
+$count = mysqli_num_rows($result);
+
+if($count == 1) {
+         echo "Login successful! Welcome ".$username."!";
+      }else {
+         echo "Your Login Name or Password is invalid ";
+}
+
+?>
