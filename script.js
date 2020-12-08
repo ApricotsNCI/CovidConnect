14f4459a090c02be9907ce240c2e6fba12ef2946
ideed <emanuelivan45@gmail.com>
2020-12-08 14:33:52 +0000

    Settings improvements

--- script.js
+++ script.js
@@ -48,10 +48,6 @@ function createUser(){
   }
 }
 
-function updateUser(){
-
-}
-
 function displayUser(){
   var username = localStorage.getItem("username");
   document.getElementById("usernameDis").innerHTML += username;
@@ -115,75 +111,3 @@ function deleteUser(){
     alert("Proccess cancelled!");
   }
 }
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
-/*ChatRoulette*/
-const socket = io('https://covid-connect-heroku.herokuapp.com/Webpages/chatroulette.html')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
-
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
-
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
-
-messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
-})
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
-
-
-Talk.ready.then(function() {
-    var me = new Talk.User({
-        id: "123456",
-        name: "Alice",
-        email: "alice@example.com",
-        photoUrl: "https://demo.talkjs.com/img/alice.jpg",
-        welcomeMessage: "Hey there! How are you? :-)"
-    });
-    window.talkSession = new Talk.Session({
-        appId: "tV6gHbbq",
-        me: me
-    });
-    var other = new Talk.User({
-        id: "654321",
-        name: "Sebastian",
-        email: "Sebastian@example.com",
-        photoUrl: "https://demo.talkjs.com/img/sebastian.jpg",
-        welcomeMessage: "Hey, how can I help?"
-    });
-
-    var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other))
-    conversation.setParticipant(me);
-    conversation.setParticipant(other);
-
-    var inbox = talkSession.createInbox({selected: conversation});
-    inbox.mount(document.getElementById("talkjs-container"));
-});
