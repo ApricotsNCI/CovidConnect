60e3842a8fc4cb6de6ae3a284b4bf0afd7399b70
ideed <emanuelivan45@gmail.com>
2020-12-09 12:22:59 +0000

    Database Push improvements

--- Phps/deleteUserphp.php
+++ Phps/deleteUserphp.php
@@ -1,11 +1,7 @@
 <?php
-/*$server = "eu-cdbr-west-03.cleardb.ne";
+$server = "eu-cdbr-west-03.cleardb.ne";
 $user = "b66c12d2a6df51";
-$pass = "42536d71";*/
-$server = "127.0.0.1:3307";
-$user = "covidconnect";
-$pass = "Eamonn00";
-$db = "heroku_ee0928b4ad437e0";
+$pass = "42536d71";
 
 $conn = new mysqli($server, $user, $pass, $db);
 
--- Phps/profileViewphp.php
+++ Phps/profileViewphp.php
@@ -1,10 +1,8 @@
 <?php
-/*$server = "eu-cdbr-west-03.cleardb.ne";
+$server = "eu-cdbr-west-03.cleardb.ne";
 $user = "b66c12d2a6df51";
-$pass = "42536d71";*/
-$server = "127.0.0.1:3307";
-$user = "covidconnect";
-$pass = "Eamonn00";
+$pass = "42536d71";
+
 $db = "heroku_ee0928b4ad437e0";
 
 $conn = new mysqli($server, $user, $pass, $db);
